3d98e3ad085f04edf03641f47dd350b4acdae8ac
wildething96 <bwilde96@yahoo.co.uk>
2021-12-19 23:15:54 +0000

    Draw Dots

--- main.js
+++ main.js
@@ -8,9 +8,41 @@ window.addEventListener("resize", () => {
   canvas.height = innerHeight;
 });
 
-// var x = canvas.width / 2;
-// var y = canvas.height / 2;
-// var dx = 2;
-// var dy = -2;
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+let hue = 0;
 
-// class
+const mouse = {
+    x: null,
+    y: null
+}
+class circleEffect {
+  constructor() {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 100;
+    this.colour;
+  }
+  update() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 30 + 20;
+    hue += 3;
+    this.colour = 'hsl(' + hue + ', 100%, 50%)';
+  }
+  draw() {
+    ctx.fillStyle = this.colour;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill()
+  }
+}
+
+let circle = new circleEffect();
+
+canvas.addEventListener('click', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y
+    circle.update();
+    circle.draw();
+})
