3c2a3d3e0fcfb236c32922964ca8157d53fde48a
wildething96 <bwilde96@yahoo.co.uk>
2021-12-20 02:44:24 +0000

    update

--- main.js
+++ main.js
@@ -8,31 +8,30 @@ window.addEventListener("resize", () => {
   canvas.height = innerHeight;
 });
 
-var x = canvas.width / 2;
-var y = canvas.height / 2;
 let hue = 0;
 particlesArray = [];
+
 const mouse = {
   x: null,
   y: null,
 };
 class circleEffect {
   constructor() {
-    // this.x = x;
-    // this.y = y;
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 20 + 10;
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // this.x = Math.random() * canvas.width;
+    // this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
-    this.colour;
+    this.colour = "hsl(" + hue + ", 100%, 50%)";
   }
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-    // this.size = Math.random() * 30 + 20;
-    hue += 0.005;
-    this.colour = "hsl(" + hue + ", 100%, 50%)";
+    hue += 0.01;
+    // this.colour = "hsl(" + hue + ", 100%, 50%)";
+    if (this.size > 0.2) this.size -= 0.1;
   }
   draw() {
     ctx.fillStyle = this.colour;
@@ -45,32 +44,44 @@ class circleEffect {
 canvas.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
+
+  for (let i = 0; i < 10; i++) {
+    particlesArray.push(new circleEffect());
+    // hue += 20;
+  }
 });
 
+// function init() {
+//   for (let i = 0; i < 100; i++) {
+//     particlesArray.push(new circleEffect());
+//   }
+// }
+// init();
 
-function init(){
-    for (let i = 0; i < 100; i++) {
-        particlesArray.push(new circleEffect())
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    
+    particlesArray[i].update();
+    particlesArray[i].draw();
+    if (particlesArray[i].x < 0 || particlesArray[i].x > canvas.width) {
+      particlesArray[i].speedX *= -1;
     }
-}
-init();
-
-function handleParticles(){
-    for (let i = 0; i < particlesArray.length; i++) {
-        if(particlesArray[i].x < 0 || particlesArray[i].x > canvas.width){
-            particlesArray[i].speedX *= -1;
-        }
-        if(particlesArray[i].y < 0 || particlesArray[i].y > canvas.height){
-            particlesArray[i].speedY *= -1;
-        }
-        particlesArray[i].update();
-        particlesArray[i].draw();
-        }
+    if (particlesArray[i].y < 0 || particlesArray[i].y > canvas.height) {
+      particlesArray[i].speedY *= -1;
+    }
+    if (particlesArray[i].size <= 0.5) {
+      particlesArray.splice(i, 1);
+      i--;
+    }
+  }
 }
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0,0,0,0.1)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
+//   hue += 0.2;
   requestAnimationFrame(animate);
 }
 animate();
